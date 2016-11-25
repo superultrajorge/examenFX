a1ed72354eae8cdecee63c39fda1767bec7a3cca
jorge <jorge@10.34.176.128>
2016-11-25 12:39:34 +0100

    d

---
+++ ExamenJAVAFX/src/Clase/MainFX.java
@@ -0,0 +1,78 @@
+package Clase;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+
+
+
+public class MainFX extends Application {
+	
+	private BorderPane rootLayout;	
+	/**
+	 * my Stage is public
+	 */
+	public Stage primaryStage;
+
+	
+	/**
+	 * sets the stage
+	 */
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			this.primaryStage = primaryStage;
+			this.primaryStage.setTitle("My GitHub Profile");
+			this.primaryStage.setX(200);
+			this.primaryStage.setY(200);
+
+			primaryStage.show();
+			
+			initRootLayout();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * loads the layout
+	 */
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainFX.class.getResource("Profile.fxml"));
+			rootLayout = (BorderPane) loader.load();
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout, 600, 400);
+			scene.getStylesheets().add(
+					getClass().getResource("estilo.css").toExternalForm());
+	
+			//			adding fonts
+			scene.getStylesheets().add("http://fonts.googleapis.com/css?family=Shadows+Into+Light");
+            Font.loadFont(getClass().getResourceAsStream("../resources/fonts/HipsterishFontNormal.ttf"), 20);
+            
+			primaryStage.setScene(scene);
+			
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param args launching 
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ ExamenJAVAFX/src/Clase/OneController.java
@@ -0,0 +1,58 @@
+package Clase;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class OneController {
+	
+	//not using yet
+	@FXML
+	private ImageView imageRaw;
+
+
+	/**
+     * Opens an stage to send the profile
+     */
+    
+    public void showSendProfile() {
+        try {
+            // Load the fxml file and create a new stage for the popup.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainFX.class.getResource("Vista/Send.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+            Stage sendStage = new Stage();
+            sendStage.setTitle("Send");
+            Scene scene = new Scene(page);
+             sendStage.setScene(scene);
+
+            
+            sendStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Method to handle the ImageView action in the Main stage 
+     */
+    
+    @FXML
+    private void handleSend() {
+    	showSendProfile();
+    }
+
+    /**
+     * Closes the application when clicked on Exit in the secondary stage
+     */
+    @FXML
+    private void handleClose() {
+   	        System.exit(0);
+    }
+    }
