670ece9329dbdb852a9d20217243863a191307f0
jorge <jorge@10.34.176.128>
2016-11-25 13:24:02 +0100

    fun

--- ExamenJAVAFX/src/Clase/OneController.java
+++ ExamenJAVAFX/src/Clase/OneController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
@@ -25,8 +26,8 @@ public class OneController {
         try {
             // Load the fxml file and create a new stage for the popup.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainFX.class.getResource("Vista/Send.fxml"));
-            AnchorPane page = (AnchorPane) loader.load();
+            loader.setLocation(MainFX.class.getResource("Send.fxml"));
+            BorderPane page = (BorderPane) loader.load();
             Stage sendStage = new Stage();
             sendStage.setTitle("Send");
             Scene scene = new Scene(page);
